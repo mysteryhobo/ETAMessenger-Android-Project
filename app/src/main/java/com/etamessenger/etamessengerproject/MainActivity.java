4a2c555ff2e65921216043bf6f117e0d90c5b851
mysteryhobo <thepeterlittle@gmail.com>
2016-05-28 00:37:06 -0400

    initial commit

--- app/src/main/java/com/etamessenger/etamessengerproject/MainActivity.java
+++ app/src/main/java/com/etamessenger/etamessengerproject/MainActivity.java
@@ -2,12 +2,68 @@ package com.etamessenger.etamessengerproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mTextView = (TextView) findViewById(R.id.responseHolder);
+
+        Button MapsBtn = (Button) findViewById(R.id.btn_maps);
+        assert MapsBtn != null;
+        MapsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+//                startActivity(intent);
+                startRequest();
+            }
+        });
+    }
+
+    public void startRequest() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mTextView.setText("Response is: " + response);
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                Document doc = Jsoup.parse(response);
+                Elements elements = doc.select("form");
+//                tgt = elements.attr("action");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "onErrorResponse: " + error.toString());
+                error.printStackTrace();
+//                mTextView.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
     }
 }
